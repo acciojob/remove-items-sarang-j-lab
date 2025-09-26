5f48ae83640dcc3599af967bd9061d8be5b037e2
Sarang <149196971+sarang-j-lab@users.noreply.github.com>
2025-09-26 12:39:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const removeBtn = document.querySelector("input");
+const colorSelect = document.querySelector("#colorSelect");
+
+let selectedItem = colorSelect.children[colorSelect.selectedIndex];
+
+colorSelect.addEventListener("click",(eve)=>{
+    selectedItem = eve.target.children[eve.target.selectedIndex]
+    console.dir(eve.target.children[eve.target.selectedIndex])
+})
+
+removeBtn.addEventListener("click",(eve)=>{
+    selectedItem.remove();
+})
